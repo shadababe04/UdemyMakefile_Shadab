966e43dc22cddfb490b3ca6dd987503e3a969e60
MOHAMMAD SHADAB ABEDIN <shadababe04@gmail.com>
2024-01-16 15:39:13 +0530

    Made Makefile with library

---
+++ 2_BasicStructure/main.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include "my_lib.hpp"
+using namespace std;
+
+int main()
+{
+    printScreen();
+    cout << "Reached point X" << endl;
+    return 0;
+}
---
+++ 2_BasicStructure/my_lib.cpp
@@ -0,0 +1,7 @@
+// my_lib.cpp
+#include "my_lib.hpp"
+#include <iostream>
+
+void printScreen() {
+    std::cout << "Library print executed!" << std::endl;
+}
